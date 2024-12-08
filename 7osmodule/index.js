d9a94e3a3fe0a43b97865842f96ebc3360fcf645
webOps-aman <amanhxe@gmail.com>
2024-12-08 23:34:20 +0530

    add new topic os module

--- 7osmodule/index.js
+++ 7osmodule/index.js
@@ -1,1 +1,57 @@
-const os = require('os');
+// // arch
+// const os = require('os');
+// let arch = os.arch();
+// console.log(arch);
+// //result: x64
+// ___________________
+
+
+
+// // freemem()
+// const os = require('os');
+// let freemem = os.freemem();
+// console.log(freemem);
+// console.log(freemem / 1024 / 1024 / 1024);
+// //result: 6623645696
+// _____________________
+
+
+
+// // totalmem()
+// const os = require('os');
+// let totalmem = os.totalmem();
+// // console.log(totalmem);
+// console.log(totalmem / 1024 / 1024 / 1024);
+// _________________
+
+
+
+// // hostname()
+// const os = require('os');
+// let hostname = os.hostname();
+// console.log(hostname);
+// ___________
+
+
+
+// // platform()
+// const os = require('os');
+// let platform = os.platform();
+// console.log(platform);
+// //result: win32
+// ___________
+
+
+
+// // tmpdir()
+// const os = require('os');
+// let tmpdir = os.tmpdir();
+// console.log(tmpdir);
+// ___________________
+
+
+
+// // type()
+// const os = require('os');
+// let type = os.type();
+// console.log(type);
