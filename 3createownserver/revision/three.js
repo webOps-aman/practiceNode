a6fadcbf38387aa9b322d6a062e3e3c335c91445
webOps-aman <amanhxe@gmail.com>
2024-12-13 20:54:21 +0530

    update file

--- 3createownserver/revision/three.js
+++ 3createownserver/revision/three.js
@@ -34,6 +34,41 @@
 
 
 
+// //require http
+// const http = require('http');
+// const port = 8000;
+// const hostName = "127.0.0.1";
+
+// //server create
+// const server = http.createServer((req, res) => {
+//     if(req.url === "/"){
+//         // res.setHeader("Content-Type", "text/html");
+//         res.write('<h1>Welcome to the Homepage</h1>');
+//         res.end();
+//     }else if(req.url === "/about"){
+//         res.setHeader("Content-Type", "text/plain");
+//         res.write('Welcome to the Aboutpage');
+//         res.end();
+//     }else if(req.url === "/contact"){
+//         res.setHeader("Content-Type", "text/plain");
+//         res.write('Welcome to the Contactpage');
+//         res.end();
+//     }else{
+//         res.setHeader("Content-Type", "text/plain");
+//         res.write('Oops! page not found');
+//         res.end();
+//     }
+// });
+
+// //listen server
+// server.listen(port, hostName, () => {
+//     console.log(`Listening on port ${port}`);
+// })
+// ________________
+
+
+
+
 //require http
 const http = require('http');
 const port = 8000;
@@ -54,6 +89,7 @@ const server = http.createServer((req, res) => {
         res.write('Welcome to the Contactpage');
         res.end();
     }else{
+        res.statusCode = 404;
         res.setHeader("Content-Type", "text/plain");
         res.write('Oops! page not found');
         res.end();
